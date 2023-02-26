54329fac0a626d2f7b1c1534c3de18fdacffb906
Shogo Sakuma <s.shogo.dev@gmail.com>
2023-02-26 19:51:22 +0900

    Add ViewModelObject

--- SNSMediaSearcher-iOS/Source/Model/ViewModelObject.swift
+++ SNSMediaSearcher-iOS/Source/Model/ViewModelObject.swift
@@ -6,3 +6,52 @@
 //
 
 import Combine
+import SwiftUI
+
+protocol ViewModelObject: ObservableObject {
+    associatedtype Input: InputObject
+    associatedtype Binding: BindingObject
+    associatedtype Output: OutputObject
+
+    var input: Input { get }
+    var binding: Binding { get }
+    var output: Output { get }
+}
+
+extension ViewModelObject where Binding.ObjectWillChangePublisher == ObservableObjectPublisher, Output.ObjectWillChangePublisher == ObservableObjectPublisher {
+    var objectWillChange: AnyPublisher<Void, Never> {
+        Publishers.Merge(binding.objectWillChange, output.objectWillChange).eraseToAnyPublisher()
+    }
+}
+
+// MARK: - InputObject
+protocol InputObject: AnyObject {
+}
+
+// MARK: - BindingObject
+protocol BindingObject: ObservableObject {
+}
+
+// MARK: - OutputObject
+protocol OutputObject: ObservableObject {
+}
+
+@propertyWrapper struct BindableObject<T: BindingObject> {
+
+    @dynamicMemberLookup struct Wrapper {
+        fileprivate let binding: T
+        subscript<Value>(dynamicMember keyPath: ReferenceWritableKeyPath<T, Value>) -> Binding<Value> {
+            return .init(
+                get: { self.binding[keyPath: keyPath] },
+                set: { self.binding[keyPath: keyPath] = $0 }
+            )
+        }
+    }
+
+    var wrappedValue: T
+
+    var projectedValue: Wrapper {
+        return Wrapper(binding: wrappedValue)
+    }
+
+}
