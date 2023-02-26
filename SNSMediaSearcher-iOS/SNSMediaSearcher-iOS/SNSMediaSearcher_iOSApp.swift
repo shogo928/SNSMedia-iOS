5493819d91dd7dcbf0afd72cf294c2622ca079a7
Shogo Sakuma <s.shogo.dev@gmail.com>
2023-02-26 15:27:33 +0900

    Created with MVVM directory structure

---
+++ SNSMediaSearcher-iOS/SNSMediaSearcher-iOS/SNSMediaSearcher_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNSMediaSearcher_iOSApp.swift
+//  SNSMediaSearcher-iOS
+//
+//  Created by 佐久間昇吾 on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SNSMediaSearcher_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
